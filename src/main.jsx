76bb6aa484967269ba3b3280b07a23f81eef2f48
sarulakno <117773807+sarulakno@users.noreply.github.com>
2024-03-15 17:32:50 -0500

    se crea componente home

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import 'boostrap/dist/css/boostrap.min.css'
+import 'boostrap/dist/jsc/boostrap.min.jsc'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+   
+  </React.StrictMode>,
+)
